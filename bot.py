d479d877209120b66045ccbec6ec05fa9a7f4963
muratkhan01 <ersultanmuratkhan@gmail.com>
2026-03-02 21:40:52 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -413,9 +413,11 @@ async def admin_reply(message: types.Message):
 
 async def main():
     await init_db()
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
 
     asyncio.run(main())
+
